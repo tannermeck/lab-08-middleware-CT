b05db6ea40fe085f28791b1e0b894d7c2b284461
TANNER MECK <steve.meck246@gmail.com>
2021-09-23 16:59:58 -0700

    getAll test passing and returning proper objects

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,6 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import getDogFact from '../lib/utils/dogAPI.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -14,16 +13,31 @@ describe('demo routes', () => {
       .send({ breed: 'Pug', dogname: 'Coco', fact: 'Pugs are very friendly' });
   });
   it('posts a new dog into the database and retrieves a dog fact', async () => {
-    const fact = await getDogFact();
     return request(app)
       .post('/api/resources/dogs')
-      .send({ breed: 'Golden-retriever', dogname: 'Oaklee', fact })
+      .send({ breed: 'Golden-retriever', dogname: 'Oaklee' })
       .then(response => {
         expect(response.body.id).toEqual(2);
         expect(response.body.breed).toEqual('Golden-retriever');
         expect(response.body.dogname).toEqual('Oaklee');
         expect(typeof(response.body.fact)).toBe('string');
   
+      });
+  });
+  
+  it('returns all dogs when the GET / route is called', async () => {
+    await request(app)
+      .post('/api/resources/dogs')
+      .send({ breed: 'Labrador', dogname: 'Goose' });
+    return request(app)
+      .get('/api/resources/dogs')
+      .then(response => {
+        const dogId = response.body.map(dog => dog.id);
+        const dog = response.body.map(dog => dog.breed);
+        const dogname = response.body.map(dog => dog.dogname);
+        expect(dogId).toEqual([1, 2]);
+        expect(dog).toEqual(['Pug', 'Labrador']);
+        expect(dogname).toEqual(['Coco', 'Goose']);
       });
   });
 
