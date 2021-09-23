9ba68f1c28594cf8e8b2fe18cc95dea28e531ccc
TANNER MECK <steve.meck246@gmail.com>
2021-09-23 15:47:12 -0700

    first post test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,18 +10,18 @@ describe('demo routes', () => {
   beforeEach(() => {
     return request(app)
       .post('/api/resources/dogs')
-      .send({ breed: 'Pug', dogName: 'Coco' });
+      .send({ breed: 'Pug', dogname: 'Coco' });
   });
-  
   it('posts a new dog into the database and retrieves a dog fact', () => {
     return request(app)
       .post('/api/resources/dogs')
-      .send({ breed: 'Golden-retriever', dogName: 'Oaklee' })
-      .then((response) => {
-        expect(response.body).toEqual({ id: 2, breed: 'Golden-retriever', dogName: 'Oaklee' });
+      .send({ breed: 'Golden-retriever', dogname: 'Oaklee' })
+      .then(response => {
+        console.log(response.body);
+        expect(response.body).toEqual({ id: 2, breed: 'Golden-retriever', dogname: 'Oaklee' });
       });
   });
-  
+
   afterAll(() => {
     pool.end();
   });
